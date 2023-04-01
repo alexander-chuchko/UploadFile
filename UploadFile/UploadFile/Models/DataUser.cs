08ec6a150d0c2a80e541dcb74f0f9bd460e656a5
Alexander Chuchko <69107323+alexander-chuchko@users.noreply.github.com>
2023-04-01 13:59:07 +0300

    Added DataUser model

---
+++ UploadFile/UploadFile/Models/DataUser.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UploadFile.Models
+{
+    public class DataUser
+    {
+        [Required]
+        public string? Name { get; set; }
+        [Required]
+        public string? Email { get; set; }
+        [Required]
+        public IFormFile? Picture { get; set; }
+    }
+}
